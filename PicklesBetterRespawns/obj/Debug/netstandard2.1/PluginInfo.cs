b6187c94e894d3b2a5285df32123965b5cb90e6e
pickle <iceisnice54@gmail.com>
2024-01-14 15:15:11 -0500

    edited mod description

---
+++ PicklesBetterRespawns/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace PicklesBetterRespawns
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "PicklesBetterRespawns";
+        public const string PLUGIN_NAME = "PicklesBetterRespawns";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
